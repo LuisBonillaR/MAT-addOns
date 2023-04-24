a4a57945dc6c0b732dbce0b34cbbf160cb4b179a
Luis Bonilla <43143126+LuisBonillaR@users.noreply.github.com>
2023-04-24 15:45:45 -0600

    Allows shift to less likely classes
    Interaction type and hadron multiplicity universes now allow shifting reconstructed class to third, fourth, and so on. It is controlled by the "depth" parameter.

--- MLSystematics.h
+++ MLSystematics.h
@@ -34,9 +34,9 @@ class MLIntTypeUniverse : public T {
   MLIntTypeUniverse(ChW* chw, double nsigma, double univs, int depth = -1) :
     m_rez(univs+1), m_depth(depth), T(chw, nsigma) {}
   
-   //===========================================================================
-   // Machine learning predictions related variables
-   //===========================================================================
+  //============================================================================
+  // Machine learning predictions related variables
+  //============================================================================
   virtual std::vector<double> GetUnshiftedIntTypeSoft() const {
     return T::GetIntTypeSoft();
   }
@@ -65,9 +65,8 @@ class MLIntTypeUniverse : public T {
   
   
   virtual int GetMLIntType() const override {
-/*    std::cout << m_lastEntry << std::endl;*/
     CalcMLIntTypeVars();
-/*    std::cout << m_lastEntry << "\n" << std::endl;*/
+    
     return m_intType;
   }
   
@@ -185,6 +184,16 @@ class MLIntTypeUniverse : public T {
   virtual bool IsMLShiftedIntType() const {
     return GetMLIntType() != T::GetMLIntType();
   }
+  
+  
+  //============================================================================
+  // Universe information
+  //============================================================================
+  virtual std::string ShortName() const { return "ML_Int_Type"; }
+  virtual std::string LatexName() const { return "ML Reco. Int. Type"; }
+  virtual bool IsVerticalOnly() const { return false; }
+};
+
 
 
 //==============================================================================
@@ -192,104 +201,208 @@ class MLIntTypeUniverse : public T {
 //==============================================================================
 template <class T>
 class MLMultUniverse : public T {
-   private:
-   double m_rez;
-    
-    //==========================================================================
-    // Constructor
-    //==========================================================================
-   public:
-   MLMultUniverse(PlotUtils::ChainWrapper* chw, double nsigma, double rez) :
-      m_rez(rez), T(chw, nsigma) {}
-    
-   //===========================================================================
-   // Machine learning predictions related variables
-   //===========================================================================
-   virtual std::vector<double> GetUnshiftedMultSoft() const {
-      return T::GetMultSoft();
-   }
-    
-   virtual std::vector<double> GetMultSoft() const override {
-      std::vector<double> softmax = T::GetMultSoft();
-      int maxProbIndex;
-      double maxProb;
-      
-      if (IsMLShiftedMult()) {
-         maxProbIndex = (int)T::GetMLHadMult();
-         maxProb = softmax[maxProbIndex];
-         softmax[maxProbIndex] = 0.0;
-         
-         for (unsigned int i = 0; i < softmax.size(); ++i)
-            softmax[i] /= (1 - maxProb);
-      }
-      
-      return softmax;
-   }
+  private:
+  double m_rez;
+  mutable int m_lastEntry = -1;
+  mutable int m_hadMult = -1;
+  mutable double m_nClasses = 3;
+  mutable std::vector<double> m_conf;
+  bool m_depth;
+  
+  typedef PlotUtils::ChainWrapper ChW;
    
-   virtual int GetUnshiftedMLHadMult() const override {
-       return T::GetMLHadMult();
-   }
+  //============================================================================
+  // Constructor
+  //============================================================================
+  // Inputs:
+  // chw: Chain with events
+  // nsigma = {1,...,Univs} current universe
+  // univs: Total number of universes
+  // depth: maximum number of shifts. depth = 1 allows shift to second most
+  //   likely class, depth = 2 allows shit to third most likely class, etc.
+  //   depth = -1 allows all shifts.
+  // Internal parameters:
+  //   m_rez: "resolution" of the effective confidence interval scanning
+  //============================================================================
+  // Constructor
+  //============================================================================
+  public:
+  MLMultUniverse(ChW* chw, double nsigma, double univs, int depth = -1) :
+    m_rez(univs+1), m_depth(depth), T(chw, nsigma) {}
    
-   virtual double GetMLHadMult() const override {
-      double nsigma = T::m_nsigma;
-      double sigRezRatio = nsigma / m_rez;
-      std::vector<double> probs = T::GetMultSoft();
-      double nClasses = (double)probs.size();
-      int maxProbIndex = (int)T::GetMLHadMult();
-     
+  //============================================================================
+  // Machine learning predictions related variables
+  //============================================================================
+  virtual std::vector<double> GetUnshiftedHadMultSoft() const {
+    return T::GetHadMultSoft();
+  }
+  
+  
+  virtual std::vector<double> GetHadMultSoft() const override {
+    CalcMLHadMultVars();
+    
+    return m_conf;
+  }
+  
+  
+  virtual int GetUnshiftedNMLHadMultClasses() const override{
+    return T::GetNMLHadMultClasses();
+  }
+  
+  
+  virtual int GetNMLHadMultClasses() const override {
+    return m_nClasses;
+  }
+  
+  
+  virtual int GetUnshiftedMLHadMult() const override {
+    return T::GetMLHadMult();
+  }
+  
+  
+  virtual int GetMLHadMult() const override {
+    CalcMLHadMultVars();
+    
+    return m_hadMult;
+  }
+  
+  
+  virtual bool CalcMLHadMultVars() const {
+    int currentEntry = T::GetEntry();
+    // Runs only if it has not run for the current entry
+    if (m_lastEntry != currentEntry) {
+      m_lastEntry = currentEntry;
+      
+      double sigma = T::m_nsigma;
+      double rez = m_rez;
+      double nClasses = (double)T::GetNMLHadMultClasses();
+      std::vector<double> probs = T::GetHadMultSoft();
+      int maxProbIndex = T::GetMLHadMult();
       double maxProb = probs[maxProbIndex];
-      int secondMaxProbIndex;
-      double secondMaxProb;
-      double threshold;
-      double secondThreshold;
-       
-      probs[maxProbIndex] = 0.0;
-      secondMaxProbIndex =
-         std::max_element(probs.begin(), probs.end()) - probs.begin();
-       
-      secondMaxProb = probs[secondMaxProbIndex] / (1 - maxProb);
-      threshold = (1 + sigRezRatio * (nClasses - 1)) / nClasses;
-      secondThreshold =
-         (1 + sigRezRatio * (nClasses - 2)) / (nClasses - 1);
       
-      // We need to take care of maxProb < 1/2 cases
-      double maxAvailProb = maxProb / (1 - maxProb);
-      while (secondThreshold > maxAvailProb && nsigma > 0) {
-         sigRezRatio = --nsigma / m_rez;
-         secondThreshold =
-            (1 + sigRezRatio * (nClasses - 2)) / (nClasses - 1);
+      double iRez = rez;
+      double iSigma = sigma;
+      bool hasShift = true;
+      
+      int currentDepth = 0;
+      while (hasShift && nClasses > 1 && currentDepth < m_depth) {
+        ++currentDepth;
+        double univs = 0;
+        hasShift =
+          CalcMLHadMultShift(iSigma, iRez, nClasses, probs, maxProbIndex);
+        if (hasShift) {
+          univs = 0;
+          int deleteIndex =
+            std::max_element(probs.begin(), probs.end()) - probs.begin();
+          double deleteProb = probs[deleteIndex];
+          
+          if (currentDepth < m_depth) {
+            int dummyIndex;
+            for (double jSigma = 1; jSigma < iRez; ++jSigma) {
+              bool addUniv =
+                CalcMLHadMultShift(jSigma, iRez, nClasses, probs, dummyIndex);
+              if (addUniv) {
+                ++univs;
+                if (jSigma == iSigma) iSigma = univs;
+              }
+            }
+            
+            iRez = univs + 1;
+          }
+          
+          probs[deleteIndex] = 0.0;
+          for (int index = 0; index < probs.size(); ++index)
+            probs[index] /= 1 - deleteProb;
+          maxProb = probs[maxProbIndex];
+          --nClasses;
+          
+        }
       }
       
-      if (maxProb < threshold && secondMaxProb > secondThreshold)
-         return secondMaxProbIndex;
-      
-      return (double)maxProbIndex;
-   }
-   
-   virtual bool IsMLShiftedMult() const {
-      return GetMLHadMult() != T::GetMLHadMult();
-   }
-   
-   //===========================================================================
-   // Universe information
-   //===========================================================================
-   virtual std::string ShortName() const {return "ML_Reco_FSI_Mult";}
-   virtual std::string LatexName() const {return "ML Reco. FSI Mult";}
-   virtual bool IsVerticalOnly() const {return false;}
+      m_nClasses = nClasses;
+      m_hadMult = maxProbIndex;
+      m_conf = probs;
+    }
+    
+    return true;
+  }
+  
+  
+  // sigma: number of current universe
+  // rez: total number of universes + 1
+  // nClasses: number of classes
+  // probs: vector of confidence (softmax)
+  // maxProbIndex: reconstructed class. Will be modified after running
+  // This function will be used for the calculation of alternate unverses, so
+  // the maxProbIndex will likely change
+  virtual bool CalcMLHadMultShift(double sigma, double rez, double nClasses,
+    std::vector<double> probs, int& maxProbIndex) const {
+    
+    bool hasShift = false;
+    
+    maxProbIndex =
+      std::max_element(probs.begin(), probs.end()) - probs.begin();
+    double maxProb = probs[maxProbIndex];
+    
+    int secondMaxProbIndex;
+    double secondMaxProb;
+    
+    double threshold;
+    double secondThreshold;
+    double sigRezRatio = sigma / rez;
+    
+    probs[maxProbIndex] = 0.0;
+    secondMaxProbIndex =
+      std::max_element(probs.begin(), probs.end()) - probs.begin();
+    if (nClasses == 2) 
+      secondMaxProb = 2 * probs[secondMaxProbIndex];
+    else secondMaxProb = probs[secondMaxProbIndex] / (1 - maxProb);
+    threshold = (1 + sigRezRatio * (nClasses - 1)) / nClasses;
+    secondThreshold = (1 + sigRezRatio * (nClasses - 2)) / (nClasses - 1);
+    
+    // We need to take care of maxProb < 1/2 cases
+    double maxAvailProb = maxProb / (1 - maxProb);
+    while (secondThreshold > maxAvailProb && sigma > 0) {
+      sigRezRatio = --sigma / rez;
+      secondThreshold =
+        (1 + sigRezRatio * (nClasses - 2)) / (nClasses - 1);
+    }
+    
+    if (maxProb < threshold && secondMaxProb > secondThreshold) {
+      maxProbIndex = secondMaxProbIndex;
+      hasShift = true;
+    }
+    
+    return hasShift;
+  }
+  
+  
+  virtual bool IsMLShiftedHadMult() const {
+    return GetMLHadMult() != T::GetMLHadMult();
+  }
+  
+  //============================================================================
+  // Universe information
+  //============================================================================
+  virtual std::string ShortName() const {return "ML_Reco_FSI_Mult";}
+  virtual std::string LatexName() const {return "ML Reco. FSI Mult";}
+  virtual bool IsVerticalOnly() const {return false;}
 };
+
 
 
 template <class T>
 std::map<std::string, std::vector<T*>>
   GetMLIntTypeSystMap(typename T::config_t chw, int univs) {
   std::map<std::string, std::vector<T*>> ret;
-  int depth = 0;
+  int depth = -1;
   for (double sigma = 1; sigma < (double)univs + 1; ++sigma)
     ret["ML_Int_Type"].push_back(new
       PlotUtils::MLIntTypeUniverse<T>(chw, sigma, (double)univs));
   
   return ret;
 }
+
 
 
 template <class T>
